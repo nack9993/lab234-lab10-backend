522b3adecb4dcce0ce091b77f2250fc5ad35ea4c
Unknown <nack99993@gmail.com>
2018-04-04 13:24:29 +0700

    Add midterm and final

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -20,4 +20,20 @@ public class GradeServiceImpl implements GradeService {
             return "F";
 
     }
+
+    @Override
+    public String getGrade(double midtermScore, double finalScore) {
+        double score = (midtermScore+finalScore)/2;
+        if (score > 79.5) {
+            return "A";
+        }
+        else if (score > 74.5){
+            return "B";
+        }else if (score > 59.5){
+            return "C";
+        }else if (score > 32.5) {
+            return "D";
+        } else
+            return "F";
+    }
 }
--- src/test/java/camt/se234/lab10/service/GradeServiceImplTest.java
+++ src/test/java/camt/se234/lab10/service/GradeServiceImplTest.java
@@ -1,9 +1,15 @@
 package camt.se234.lab10.service;
 
 import camt.se234.lab10.service.GradeServiceImpl;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+@RunWith(JUnitParamsRunner.class)
 public class GradeServiceImplTest {
     @Test
     public void testGetGrade(){
@@ -19,6 +25,24 @@ public class GradeServiceImplTest {
         assertThat(gradeService.getGrade(32),is("F"));
         assertThat(gradeService.getGrade(0),is("F"));
 
+        assertThat(gradeService.getGrade(80,80),is("A"));
+        assertThat(gradeService.getGrade(75,75),is("B"));
+
+    }
+
+    @Test
+    @Parameters(method = "paramsForTestGetGradeParams")
+    @TestCaseName("Test getGrade Params [{index}] : input is {0},expect \"{1}\"")
+    public void  testGetGradeparams(double score,String expectedGrade){
+        GradeServiceImpl gradeService = new GradeServiceImpl();
+        assertThat(gradeService.getGrade(score),is(expectedGrade));
+    }
+
+    public Object paramsForTestGetGradeParams(){
+        return new Object[][]{
+                {100,"A"},
+                {77,"B"}
+        };
     }
 
 }
